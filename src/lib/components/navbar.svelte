3c58124409af18b73e4787c229f4ea2d40d2f3d5
cyteon <cyteon>
2024-07-26 10:52:27 +0200

    thingy in navbar

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,5 +1,21 @@
+<script>
+    import UserButton from "clerk-sveltekit/client/UserButton.svelte";
+    import SignInButton from "clerk-sveltekit/client/SignInButton.svelte";
+    import SignedOut from "clerk-sveltekit/client/SignedOut.svelte";
+</script>
+
 <nav class="bg-ctp-surface0">
-    <div class="p-3">
+    <div class="p-3 flex flex-row">
         <a class="text-ctp-text text-xl" href="/">Rabbit</a>
+        <div class="justify-end ml-auto">
+            <UserButton />
+            <SignedOut>
+                <button
+                    class="text-ctp-crust bg-ctp-blue py-1 px-3 rounded-md transition-all hover:scale-105 duration-300 shadow-md"
+                >
+                    <SignInButton />
+                </button>
+            </SignedOut>
+        </div>
     </div>
 </nav>
