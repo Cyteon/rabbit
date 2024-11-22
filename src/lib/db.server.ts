6fc5c4d810001a9a5cb2cb9eedccd613f4beee12
Cyteon <129582290+Cyteon@users.noreply.github.com>
2024-11-22 11:33:30 +0000

    oopsies

--- src/lib/db.server.ts
+++ src/lib/db.server.ts
@@ -11,7 +11,7 @@ async function migrate() {
     subrabbits_interacted_with TEXT[] DEFAULT '{}'
   );`;
 
-  await sql`ALTER TABLE users DROP COLUMN subrabbits_interacted_with;`; // deprecating cause wack
+  await sql`ALTER TABLE users DROP COLUMN subrabbits_interacted_with IF EXISTS;`;
 
   await sql`CREATE TABLE IF NOT EXISTS sessions (
     id SERIAL PRIMARY KEY,
